f9851e771d938e864009e1e817299d810194cdb0
Rafael Bomfim <44168444+rbbomfim@users.noreply.github.com>
2022-10-03 19:04:03 -0300

    subir pasta log

--- utils/util.py
+++ utils/util.py
@@ -2,12 +2,15 @@ from logging import basicConfig, error, critical, warning, debug, info
 from logging import CRITICAL, WARNING, INFO, DEBUG, ERROR
 from logging import FileHandler, StreamHandler
 from decouple import config
+import os
 
 def log (filename):
+    log_filename = f"./logs/{filename}.txt"
+    os.makedirs(os.path.dirname(log_filename), exist_ok=True)
     basicConfig(
         level=config('NIVEL_LOG'),
             handlers=[
-            FileHandler(f"./logs/{filename}.txt",encoding = "UTF-8"), 
+            FileHandler(log_filename,encoding = "UTF-8"), 
             StreamHandler()
         ],
         format='%(asctime)s %(name)s %(levelname)s: %(message)s',
